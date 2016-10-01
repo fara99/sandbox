d86e9035d9d871edaaa11d25c733d70bd71a2a26
fara99 <far0sino@gmail.com>
2016-10-01 16:57:14 +0200

    Vlozeni sample dat V3

--- store.sql
+++ store.sql
@@ -1,4 +1,5 @@
-﻿-- struktura databaze
+﻿
+-- struktura databaze
 
 CREATE TABLE store(
 id int primary key
@@ -16,30 +17,31 @@ CREATE INDEX fk_store_id_idx ON stock(store_id);
 CREATE INDEX fk_product_id_idx ON stock(product_id);
 
 -- vlozeni sample dat
-INSERT INTO store VALUES(0, 'Obchod 1');
-INSERT INTO store VALUES(1, 'Obchod 2');
-INSERT INTO store VALUES(2, 'Obchod 3');
+INSERT INTO store VALUES
+(0, 'Obchod 1'),
+(1, 'Obchod 2'),
+(2, 'Obchod 3');
 
-INSERT INTO product VALUES(0, 'Produkt 1');
-INSERT INTO product VALUES(1, 'Produkt 2');
-INSERT INTO product VALUES(2, 'Produkt 3');
-INSERT INTO product VALUES(3, 'Produkt 4');
-INSERT INTO product VALUES(4, 'Produkt 5');
+INSERT INTO product VALUES
+(0, 'Produkt 1'),
+(1, 'Produkt 2'),
+(2, 'Produkt 3'),
+(3, 'Produkt 4'),
+(4, 'Produkt 5');
 
-INSERT INTO stock VALUES(0,0,5);
-INSERT INTO stock VALUES(0,1,3);
-INSERT INTO stock VALUES(0,2,4);
-INSERT INTO stock VALUES(0,3,0);
-INSERT INTO stock VALUES(0,4,2);
-
-INSERT INTO stock VALUES(1,0,1);
-INSERT INTO stock VALUES(1,1,2);
-INSERT INTO stock VALUES(1,2,3);
-INSERT INTO stock VALUES(1,3,4);
-INSERT INTO stock VALUES(1,4,5);
-
-INSERT INTO stock VALUES(2,0,5);
-INSERT INTO stock VALUES(2,1,4);
-INSERT INTO stock VALUES(2,2,3);
-INSERT INTO stock VALUES(2,3,2);
-INSERT INTO stock VALUES(2,4,1);
+INSERT INTO stock VALUES
+(0,0,5),
+(0,1,3),
+(0,2,4),
+(0,3,0),
+(0,4,2),
+(1,0,1),
+(1,1,2),
+(1,2,3),
+(1,3,4),
+(1,4,5),
+(2,0,5),
+(2,1,4),
+(2,2,3),
+(2,3,2),
+(2,4,1);
