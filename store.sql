243e6fe5adbe21664fc51bcda1adf862252325de
fara99 <far0sino@gmail.com>
2016-10-01 15:31:37 +0200

    Vytvoreni struktury databaze dle zadani

---
+++ store.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE store(id integer primary key, store_name varchar(50));
+CREATE TABLE product(id integer primary key, product_name varchar(50));
+CREATE TABLE stock(store_id integer references store(id),product_id integer references product(id), amount integer);
+CREATE UNIQUE INDEX fk_store_id_idx ON stock(store_id);
+CREATE UNIQUE INDEX fk_product_id_idx ON stock(product_id);
